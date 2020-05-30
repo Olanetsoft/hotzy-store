323bf093c3806eb6db3092f55f224d3eeab2b984
olanetsoft <heedris2olubisi@gmail.com>
2020-05-30 13:57:51 +0100

    DID SOME REFACTORING, BCRYPT ALSO INSTALLED

---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+//importing cypto from the node module
+const crypto = require('crypto')
+
+//importing bcrypt
+const bcrypt = require('bcrypt');
+
