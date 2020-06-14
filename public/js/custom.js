2977b39536b642fd3a58d5b8a9b6331f75d7d694
olanetsoft <heedris2olubisi@gmail.com>
2020-06-14 11:06:54 +0100

    FIXED SEVERAL CONSOLE BUG

--- public/js/custom.js
+++ public/js/custom.js
@@ -354,9 +354,6 @@ inputNumber($('.input-number'));
   });  
  }
     
-   // Hide default stripe button, be careful there if you
-    // have more than 1 button of that class
-    document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
-
+   
 
 }(jQuery));
---
+++ public/js/customStripeButton.js
@@ -0,0 +1,3 @@
+// Hide default stripe button, be careful there if you
+// have more than 1 button of that class
+document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
